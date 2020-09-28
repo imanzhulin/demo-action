79a4bdb098e38557f5022550d7564231114fa50b
Ivan Manzhulin <imanzhulin@certicasolutions.com>
2020-09-28 18:36:03 +0300

    Added firewall rules

--- digitalocean_droplet.tf
+++ digitalocean_droplet.tf
@@ -18,3 +18,44 @@ resource "digitalocean_project" "infra" {
   purpose     = "Operational / Developer tooling"
   resources   = [digitalocean_droplet.do-droplet.urn]
 }
+
+resource "digitalocean_firewall" "do-droplet" {
+  name = "allow-ssh-http-https"
+
+  droplet_ids = [digitalocean_droplet.do-droplet.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
